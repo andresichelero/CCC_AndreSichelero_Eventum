d9be73d8e39f34b80d8a97b2cbbfa1de4df7f5b5
Andre Sichelero <andre.gasolisichelero@rwdi.com>
2025-10-12 19:12:09 -0300

    Adicionadas classes de formulário para login, registro e eventos

---
+++ app/forms.py
@@ -0,0 +1,56 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    TextAreaField,
+    DateTimeField,
+    PasswordField,
+    SubmitField,
+    SelectField,
+)
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    submit = SubmitField("Entrar")
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField(
+        "Nome Completo", validators=[DataRequired(), Length(min=3, max=150)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    # Papel do usuário: 1=Organizador, 2=Palestrante/Autor, 3=Participante
+    role = SelectField(
+        "Eu sou",
+        choices=[
+            ("3", "Participante"),
+            ("2", "Palestrante/Autor"),
+            ("1", "Organizador"),
+        ],
+        validators=[DataRequired()],
+    )
+    password = PasswordField("Senha", validators=[DataRequired(), Length(min=6)])
+    password2 = PasswordField(
+        "Repetir Senha",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="As senhas devem ser iguais."),
+        ],
+    )
+    submit = SubmitField("Registrar")
+
+
+class EventForm(FlaskForm):
+    title = StringField(
+        "Título do Evento", validators=[DataRequired(), Length(max=250)]
+    )
+    description = TextAreaField("Descrição")
+    start_date = DateTimeField(
+        "Data de Início", format="%d/%m/%Y %H:%M", validators=[DataRequired()]
+    )
+    end_date = DateTimeField(
+        "Data de Fim", format="%d/%m/%Y %H:%M", validators=[DataRequired()]
+    )
+    submit = SubmitField("Salvar Evento")
